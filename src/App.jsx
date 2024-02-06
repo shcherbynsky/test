b20e0f421104e4ba08cbbdb4562db54428c12bf0
shcherbynsky <shcherbynsky@gmail.com>
2024-02-06 18:04:28 +0200

    change url case

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/css/app.css'
 import Header from './components/header/Header'
-import Main from './components/main/main';
+import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 
 
